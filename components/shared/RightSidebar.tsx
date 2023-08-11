010bbfd4e277b06857afddf4b1d2e09e29f985e4
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-08-11 13:42:54 +0330

    feat: add righsidebar placeholder

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1 capitalize">
+          suggested communites
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1 capitalize">
+          suggested users
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
