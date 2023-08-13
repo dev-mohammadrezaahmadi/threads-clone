e2d037fb6b07f2c2388a61e78ab1ed17def89bae
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-08-14 03:16:30 +0330

    feat: add fetch thread by id

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -40,7 +40,6 @@ export async function createThread({
   }
 }
 
-
 export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
@@ -75,4 +74,47 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   const isNext = totalPostsCount > skipAmount + posts.length;
 
   return { posts, isNext };
+}
+
+export async function fetchThreadById(threadId: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      }) // Populate the author field with _id and username
+      // .populate({
+      //   path: "community",
+      //   model: Community,
+      //   select: "_id id name image",
+      // }) // Populate the community field with _id and name
+      .populate({
+        path: "children", // Populate the children field
+        populate: [
+          {
+            path: "author", // Populate the author field within children
+            model: User,
+            select: "_id id name parentId image", // Select only _id and username fields of the author
+          },
+          {
+            path: "children", // Populate the children field within children
+            model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+            populate: {
+              path: "author", // Populate the author field within nested children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (err) {
+    console.error("Error while fetching thread:", err);
+    throw new Error("Unable to fetch thread");
+  }
 }
