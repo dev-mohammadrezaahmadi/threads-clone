ea740a0aec9102df52be5be874a1845447389f0e
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-08-14 03:10:52 +0330

    feat: show threads on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,42 @@
-import { UserButton } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  
+  const result = await fetchPosts(1, 30);
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p>No Threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post, i) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
   );
 }
