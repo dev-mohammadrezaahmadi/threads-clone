d2f1f7ba38ac7a5472c101a71cbe51e531cbd4cb
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-08-16 02:07:26 +0330

    fix: update props

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,55 +1,66 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
-import { Button } from "../ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
-  pageNumber: number;
-  isNext: boolean;
-  path: string;
+  accountId: string;
+  authUserId: string;
+  name: string;
+  username: string;
+  imgUrl: string;
+  bio: string;
+  type?: string;
 }
 
-function Pagination({ pageNumber, isNext, path }: Props) {
-  const router = useRouter();
-
-  const handleNavigation = (type: string) => {
-    let nextPageNumber = pageNumber;
-
-    if (type === "prev") {
-      nextPageNumber = Math.max(1, pageNumber - 1);
-    } else if (type === "next") {
-      nextPageNumber = pageNumber + 1;
-    }
-
-    if (nextPageNumber > 1) {
-      router.push(`/${path}?page=${nextPageNumber}`);
-    } else {
-      router.push(`/${path}`);
-    }
-  };
-
-  if (!isNext && pageNumber === 1) return null;
-
+function ProfileHeader({
+  accountId,
+  authUserId,
+  name,
+  username,
+  imgUrl,
+  bio,
+  type,
+}: Props) {
   return (
-    <div className="pagination">
-      <Button
-        onClick={() => handleNavigation("prev")}
-        disabled={pageNumber === 1}
-        className="!text-small-regular text-light-2"
-      >
-        Prev
-      </Button>
-      <p className="text-small-semibold text-light-1">{pageNumber}</p>
-      <Button
-        onClick={() => handleNavigation("next")}
-        disabled={!isNext}
-        className="!text-small-regular text-light-2"
-      >
-        Next
-      </Button>
+    <div className='flex w-full flex-col justify-start'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <div className='relative h-20 w-20 object-cover'>
+            <Image
+              src={imgUrl}
+              alt='logo'
+              fill
+              className='rounded-full object-cover shadow-2xl'
+            />
+          </div>
+
+          <div className='flex-1'>
+            <h2 className='text-left text-heading3-bold text-light-1'>
+              {name}
+            </h2>
+            <p className='text-base-medium text-gray-1'>@{username}</p>
+          </div>
+        </div>
+        {accountId === authUserId && type !== "Community" && (
+          <Link href='/profile/edit'>
+            <div className='flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2'>
+              <Image
+                src='/assets/edit.svg'
+                alt='logout'
+                width={16}
+                height={16}
+              />
+
+              <p className='text-light-2 max-sm:hidden'>Edit</p>
+            </div>
+          </Link>
+        )}
+      </div>
+
+      <p className='mt-6 max-w-lg text-base-regular text-light-2'>{bio}</p>
+
+      <div className='mt-12 h-0.5 w-full bg-dark-3' />
     </div>
   );
 }
 
-export default Pagination;
+export default ProfileHeader;
